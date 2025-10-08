4fbe13b0ee2758cc10ba15a4bcb7abe74ddf92eb
khmerweb <vuthdevelop1@gmail.com>
2025-10-08 08:56:05 +0700

    Test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,19 @@
     <script>
         import { base } from '$app/paths';
-        //import { loadDatabase } from '$lib/db/db.svelte.js';
+        import { loadDatabase } from '$lib/db/db.svelte.js';
         
         let worker;
         let queryResult = $state(null);
         let errorMessage = $state(null);
         let formattedData = $state([]);
 
-        async function loadDatabase(base) {
-    const worker = new Worker(new URL('/src/lib/db/worker.js', import.meta.url), { type: 'module' });
-            worker.onmessage = (e) => {
-                const { type, results, error } = e.data;
-                if (type === 'init_success') {
-                    console.log('Database initialized in worker');
-                    worker.postMessage({ type: 'query', payload: { sql: 'SELECT * FROM Post' } });
-                } else if (type === 'init_error') {
-                    errorMessage = `Worker initialization failed: ${error}`;
-                } else if (type === 'query_success') {
-                    queryResult = results;
-                } else if (type === 'query_error') {
-                    errorMessage = `Query failed: ${error}`;
-                }
-            };
-            
-            const getBasePath = () => {
-                const pathParts = window.location.pathname.split('/');
-                if (pathParts.length > 1 && pathParts[1] === 'video') {
-                    return `/${pathParts[1]}/`;
-                }
-                return '/'; 
-            };
-
-            worker.postMessage({ type: 'init', payload: { dbPath: `${base}/database.sqlite` }, basePath: getBasePath() });
-            
-            const columns = queryResult[0].columns;
-            const values = queryResult[0].values;
-
-            formattedData = await values.map((row) => {
-                const rowObject = {};
-                columns.forEach((colName, index) => {
-                    rowObject[colName] = row[index];
-                });
-                return rowObject;
-            });
-
-            worker.terminate();
-
-        return formattedData;
-}
+        
 
         $effect(async () => {
-            await loadDatabase(base);
+            formattedData = await loadDatabase(base);
         });
     </script>
-
+{formattedData.length}
     {#if formattedData.length > 0}
         <pre>{JSON.stringify(formattedData, null, 2)}</pre>
     {:else}
