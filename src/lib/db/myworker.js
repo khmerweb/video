2aa2a5d3c0e7883c59c5911146f9991c1d9ca551
khmerweb <vuthdevelop1@gmail.com>
2025-10-12 13:48:44 +0700

    Make it better

--- src/lib/db/myworker.js
+++ src/lib/db/myworker.js
@@ -11,7 +11,8 @@ self.onmessage = async (event) => {
                 const SQL = await initSqlJs({
                     locateFile: file => `${basePath}/${file}` 
                 });
-                const response = await fetch(payload.dbPath, {headers: {"Cache-Control": "max-age=86400"}});
+                //{headers: {"Cache-Control": "max-age=7200"}}
+                const response = await fetch(payload.dbPath, {headers: {"Cache-Control": "max-age=7200"}});
                 const buffer = await response.arrayBuffer();
                 db = new SQL.Database(new Uint8Array(buffer));
                 self.postMessage({ type: 'init_success' });
